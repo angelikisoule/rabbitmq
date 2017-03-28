7f0fbce129bebfce03b35f600607d9c88d87247d
angelikisoule <angelikisoule@gmail.com>
2017-03-29 00:27:02 +0300

    ADD: send user message

--- src/main/java/rabbitmq/UserMessageManager.java
+++ src/main/java/rabbitmq/UserMessageManager.java
@@ -1,15 +1,20 @@
 package rabbitmq;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
 
 import javax.inject.Inject;
 
-import com.rabbitmq.client.AMQP.Exchange.BindOk;
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.AMQP.Queue.BindOk;
 import com.rabbitmq.client.Channel;
 
 public class UserMessageManager {
-	
+
 	static final String USER_INBOXES_EXCHANGE = "user-inboxes";
+	static final String MESSAGE_CONTENT_TYPE = "application/vnd.ccm.pmsg.v1+json";
+	static final String MESSAGE_ENCODING = "UTF-8";
 
 	@Inject
 	RabbitMqManager rabbitMqManager;
@@ -20,12 +25,52 @@ public class UserMessageManager {
 		rabbitMqManager.call(callableImpl);
 	}
 
-	public void onUserLogin(final long userId){
-	       final String queue = getUserInboxQueue(userId);
-	       ChannelCallableImplBindOk bindOk = new ChannelCallableImplBindOk();
-	       bindOk.setDescription("Declaring user queue: " + queue + ", binding it to exchange: " + USER_INBOXES_EXCHANGE);
-	       rabbitMqManager.call(bindOk);
-	               
+	public void onUserLogin(final long userId) {
+		final String queue = getUserInboxQueue(userId);
+		ChannelCallableImplBindOk bindOk = new ChannelCallableImplBindOk();
+		bindOk.setDescription("Declaring user queue: " + queue + ", binding it to exchange: " + USER_INBOXES_EXCHANGE);
+		rabbitMqManager.call(bindOk);
+	}
+
+	public String getUserInboxQueue(long userId) {
+		return "user-inbox." + userId;
+	}
+
+	public String sendUserMessage(final long userId, final String jsonMessage) {
+		
+		return rabbitMqManager.call(new ChannelCallable<String>() {
+			public String getDescription() {
+				return "Sending message to user: " + userId;
+			}
+
+			public String call(final Channel channel) throws IOException {
+				String queue = getUserInboxQueue(userId);
+				// it may not exist so declare it
+				declareUserMessageQueue(queue, channel);
+				String messageId = UUID.randomUUID().toString();
+				BasicProperties props = new BasicProperties.Builder().contentType(MESSAGE_CONTENT_TYPE)
+						.contentEncoding(MESSAGE_ENCODING).messageId(messageId).deliveryMode(2).build();
+				String routingKey = queue;
+				// publish the message to the direct exchange
+				channel.basicPublish(USER_INBOXES_EXCHANGE, routingKey, props, jsonMessage.getBytes(MESSAGE_ENCODING));
+				return messageId;
+			}
+		});
+	}
+	
+	private BindOk declareUserMessageQueue(final String queue, final Channel channel) throws IOException {
+		// survive a server restart
+		boolean durable = true;
+		// keep the queue
+		boolean autoDelete = false;
+		// can be consumed by another connection
+		boolean exclusive = false;
+		// no special arguments
+		Map<String, Object> arguments = null;
+		channel.queueDeclare(queue, durable, exclusive, autoDelete, arguments);
+		// bind the addressee's queue to the direct exchange
+		String routingKey = queue;
+		return channel.queueBind(queue, USER_INBOXES_EXCHANGE, routingKey);
 	}
 
 }
